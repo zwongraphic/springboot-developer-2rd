b79215fda36ad8b8ca53128d4e2e0ba166ce598b
zwongraphic <bababaenjooo@gmail.com>
2026-02-12 11:11:55 +0900

    ch3-structure/ch4-test

--- src/main/java/me/leejiwon/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/leejiwon/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeveloperApplication.class);
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
---
+++ src/test/java/JUnitCycleQuiz.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitCycleQuiz {
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Hello!");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Bye!");
+    }
+
+    @Test
+    public void junitQuiz3() {
+        System.out.println("This is first test");
+    }
+
+    @Test
+    public void junitQuiz4() {
+        System.out.println("This is second test");
+    }
+}
