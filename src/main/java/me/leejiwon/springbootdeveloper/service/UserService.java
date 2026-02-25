e80f379eff3a37d43584931c01bd8bd0a4d37814
zwongraphic <bababaenjooo@gmail.com>
2026-02-25 21:11:11 +0900

    ch8-security

---
+++ src/main/java/me/leejiwon/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.leejiwon.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leejiwon.springbootdeveloper.domain.User;
+import me.leejiwon.springbootdeveloper.dto.AddUserRequest;
+import me.leejiwon.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
